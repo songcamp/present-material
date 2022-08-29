9fe602ce7ae239ab219559967761491c048f4ab3
Jem Gold <591643+jongold@users.noreply.github.com>
2022-08-29 11:44:07 -0400

    Update with Max's changes

--- contracts/src/CurationSeason.sol
+++ contracts/src/CurationSeason.sol
@@ -1,24 +1,31 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.10;
+pragma solidity ^0.8.13;
 
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/token/ERC721/IERC721.sol";
 
-/// @title CurationSeason
-/// @notice Explain to an end user what this does
-contract CurationSeason is Ownable {
+/// @title CurationManager
+/// @notice Facilitates on-chain curation of a dynamic array of ethereum addresses
+contract CurationManager is Ownable {
     /* ===== ERRORS ===== */
+
     /// @notice invalid curation pass
     error Access_MissingPass();
 
     /// @notice
     error Access_Unauthorized();
 
-    /// @notice season is inactive
+    /// @notice curation is inactive
     error Inactive();
+
+    /// @notice curation is finalized
+    error Finalized();
 
     /// @notice duplicate listing
     error ListingAlreadyExists();
+
+    /// @notice exceeding curation limit
+    error CurationLimitExceeded();
 
     /* ===== EVENTS ===== */
     event ListingAdded(address indexed curator, address indexed listingAddress);
@@ -32,7 +39,11 @@ contract CurationSeason is Ownable {
 
     event CurationPassUpdated(address indexed sender, address curationPass);
 
-    event SeasonFinalized(address sender);
+    event CurationPaused(address sender);
+
+    event CurationResumed(address sender);
+
+    event CurationFinalized(address sender);
 
     /* ===== VARIABLES ===== */
 
@@ -41,7 +52,10 @@ contract CurationSeason is Ownable {
 
     string public title;
     IERC721 public curationPass;
-    bool public isActive = true;
+    bool public isActive;
+    bool public isFinalized = false;
+    // unlimited curation limit if set to 0
+    uint256 public curationLimit;
 
     /* ===== MODIFIERS ===== */
 
@@ -64,11 +78,41 @@ contract CurationSeason is Ownable {
         _;
     }
 
+    // checks if curation functionality is finalized
+    modifier onlyIfFinalized() {
+        if (isFinalized == true) {
+            revert Finalized();
+        }
+
+        _;
+    }
+
+    // checks if curation limit has been reached
+    modifier onlyIfLimit() {
+        if (curationLimit != 0 && listings.length == curationLimit) {
+            revert CurationLimitExceeded();
+        }
+
+        _;
+    }
+
     /* ===== CONSTRUCTOR ===== */
 
-    constructor(string memory _title, IERC721 _curationPass) {
+    constructor(
+        string memory _title,
+        IERC721 _curationPass,
+        uint256 _curationLimit,
+        bool _isActive
+    ) {
         title = _title;
         curationPass = _curationPass;
+        curationLimit = _curationLimit;
+        isActive = _isActive;
+        if (isActive == true) {
+            emit CurationResumed(_msgSender());
+        } else {
+            emit CurationPaused(_msgSender());
+        }
     }
 
     /* ===== CURATION FUNCTIONS ===== */
@@ -79,6 +123,7 @@ contract CurationSeason is Ownable {
         external
         onlyIfActive
         onlyOwnerOrCurator
+        onlyIfLimit
     {
         if (listingCurators[listing] != address(0)) {
             revert ListingAlreadyExists();
@@ -128,13 +173,85 @@ contract CurationSeason is Ownable {
         emit CurationPassUpdated(_msgSender(), address(_curationPass));
     }
 
-    function finalizeSeason() public onlyOwner {
-        isActive = false;
+    function updateCurationLimit(uint256 _newLimit) public onlyOwner {
+        require(
+            _newLimit > listings.length,
+            "cannot set curationLimit to value equal to or smaller than current length of listings array"
+        );
+        curationLimit = _newLimit;
 
-        emit SeasonFinalized(_msgSender());
+        // maybe add an emitted event here
     }
 
-    /* ===== HELPERS ===== */
+    function flipIsActiveBool() public onlyOwner {
+        if (isActive == true) {
+            isActive = false;
+            emit CurationPaused(_msgSender());
+        } else {
+            isActive = true;
+            emit CurationResumed(_msgSender());
+        }
+    }
+
+    function finalizeCuration() public onlyOwner {
+        if (isActive == false) {
+            isFinalized == true;
+            emit CurationFinalized(_msgSender());
+            return;
+        }
+
+        isActive = false;
+        emit CurationPaused(_msgSender());
+
+        isFinalized = true;
+        emit CurationFinalized(_msgSender());
+    }
+
+    // add listing functionality without isActive check
+    function onwerAddListing(address listing)
+        external
+        onlyIfLimit
+        onlyIfFinalized
+        onlyOwner
+    {
+        if (listingCurators[listing] != address(0)) {
+            revert ListingAlreadyExists();
+        }
+
+        require(
+            listing != address(0),
+            "listing address cannot be the zero address"
+        );
+
+        listingCurators[listing] = _msgSender();
+
+        listings.push(listing);
+
+        emit ListingAdded(_msgSender(), listing);
+    }
+
+    /// remove listing functionality without isActive or Access_Unauthorized check
+    function ownerRemoveListing(address listing)
+        external
+        onlyIfFinalized
+        onlyOwner
+    {
+        delete listingCurators[listing];
+        removeByValue(listing);
+
+        emit ListingRemoved(_msgSender(), listing);
+    }
+
+    /* ===== VIEW FUNCTIONS ===== */
+
+    // view function that returns array of all active listings
+    function viewAllListings() external view returns (address[] memory) {
+        // returns empty array if no active listings
+        return listings;
+    }
+
+    /* ===== INTERNAL HELPERS ===== */
+
     function find(address value) internal view returns (uint256) {
         uint256 i = 0;
         while (listings[i] != value) {
